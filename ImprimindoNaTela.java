2581a601a42eb51aaac343f514023ef95b5f867b
Lucas Ribeiro <proftilucas@outlook.com>
2022-10-20 22:22:16 -0300

    Introdução de como imprimir na tela no Java

---
+++ ImprimindoNaTela.java
@@ -0,0 +1,7 @@
+public class ImprimindoNaTela { // ImprimindoNaTela é o nome da classe, e está definida como público.
+
+    public static void main(String[] args) { // tornar a classe executável.
+
+        System.out.println("Olá, mundo!"); // System.out.println() é necessário para imprimir algo na tela.
+    }
+}
