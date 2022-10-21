f66aab0e392decf3a7c44d72fd6e7d278a89193c
Lucas Ribeiro <proftilucas@outlook.com>
2022-10-20 22:29:37 -0300

    Realizando o primeiro exercício de impressão em tela utilizando quebra de linha \n

---
+++ Exercicio1.java
@@ -0,0 +1,14 @@
+/** Altere seu programa para imprimir uma mensagem	diferente.
+ *Altere seu programa para imprimir duas linhas de texto usando duas linhas de código System.out.
+ * Sabendo que os caracteres \n representam uma quebra de linhas, imprima duas	linhas de texto
+ *usando uma única	linha de código System.out.
+ */
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo! Meu nome é Lucas!");
+        System.out.println("Eu tenho 23 anos! \nUm pouco velho, não é mesmo?");
+
+    }
+}
