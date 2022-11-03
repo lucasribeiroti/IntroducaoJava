36357f979dd360bfde9ef45d119f61d8f14e29fa
Lucas de Paiva Ribeiro <lucas@flaire.local>
2022-11-03 10:44:05 -0300

    Organização em packages

--- ImprimindoNaTela.java
+++
@@ -1,7 +0,0 @@
-public class ImprimindoNaTela { // ImprimindoNaTela é o nome da classe, e está definida como público.
-
-    public static void main(String[] args) { // tornar a classe executável.
-
-        System.out.println("Olá, mundo!"); // System.out.println() é necessário para imprimir algo na tela.
-    }
-}
