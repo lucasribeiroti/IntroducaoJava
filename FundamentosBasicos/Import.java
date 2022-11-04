f4808d95b12d19de12c419ba57f19823171c3b10
Lucas Ribeiro <proftilucas@outlook.com>
2022-11-03 21:09:45 -0300

    Import

---
+++ FundamentosBasicos/Import.java
@@ -0,0 +1,23 @@
+package FundamentosBasicos;
+
+import javax.swing.*;
+import java.util.Date;
+
+public class Import {
+
+    public static void main(String[] args) {
+
+        java.lang.String b = "Boa tarde!";
+        System.out.println(b);
+
+        java.lang.System.out.println(b);
+
+        String s = "Olá!";
+        System.out.println(s);
+
+        Date d = new Date();
+        System.out.println(d);
+
+        JButton botao = new JButton();
+    }
+}
