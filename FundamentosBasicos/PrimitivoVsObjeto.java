c70ab40e9d76727976ea5572b9543645a5e6f54b
Lucas Ribeiro <proftilucas@outlook.com>
2022-11-05 16:42:05 -0300

    Primitivos vs Objeto

---
+++ FundamentosBasicos/PrimitivoVsObjeto.java
@@ -0,0 +1,21 @@
+package FundamentosBasicos;
+
+public class PrimitivoVsObjeto {
+
+    public static void main(String[] args) {
+
+        String s = "Qualquer coisa";
+        s = s.toUpperCase();
+        System.out.println(s);
+
+        // Wrappers: são a versão "objeto" dos tipos primitivos.
+        int a = 123;
+        System.out.println(a);
+    }
+}
+/*
+Tudo em Java é objeto, menos os 8 tipos primitivos que temos:
+byte, short, int, long, float, double, char e bool.
+Quando criamos objetos em determinadas classes, temos possibilidade de atributos em comportamentos do
+tipo primitivo.
+*/
